a311675b4e5c58781b5c84e6a7acdbce7b3f8457
nicksolony <nicksolony@gmail.com>
2020-04-28 23:52:22 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,13 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    if query
+      Student.where("name LIKE ? ", "%#{query}%").sort_by(&:name)
+    else
+      Student.all.sort_by(&:name)
+    end
+  end
+
 end
